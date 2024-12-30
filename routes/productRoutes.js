fe92e763e0e0a8b3cf09a04e6e9b903cb72f64cf
Vishwa1011 <vishupatel1011@gmail.com>
2024-12-30 16:50:43 +0530

    No Syntactical Errors

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const { signQRCodeBatch, updateBatchProducts, scanQRCodeUnified } = require('../controllers/productController');
+const { signQRCodeBatch, scanQRCodeUnified } = require('../controllers/productController');
 const router = express.Router();
 
 // Route for signing a batch of QR codes and generating a master QR code
 router.post('/sign', signQRCodeBatch);
-
-// Route for updating products in a batch with a master QR code scan location
-router.post('/updateBatch', updateBatchProducts);
 
 // Route for scanning individual product or master QR codes
 router.post('/scan', scanQRCodeUnified);
